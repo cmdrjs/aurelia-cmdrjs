b613bc2a321ff35daeda7723d6744d179681c1d6
cruikshj <cruikshank@Cruikshank10.zywave.com>
2016-01-07 15:19:06 -0600

    options and import fix

--- src/cmdr-element.js
+++ src/cmdr-element.js
@@ -1,16 +1,19 @@
-import Shell from 'cmdrjs/cmdrjs'
+import {Shell} from 'cmdrjs'
 import {customElement, bindable, inject} from 'aurelia-framework';
 
 @customElement('cmdr')
 @inject(Element)
 export class CmdrElement {
+    
+    @bindable options = {};
+    
     constructor(element) {
         this.element = element;
         this.shell = null;
     }
 
     attached() {
-        this.shell = new Shell(this.element);
+        this.shell = new Shell(this.element, this.options);
     }
     
     detached() {
